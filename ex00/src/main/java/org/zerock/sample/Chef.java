96ab53ecde48c37c33d243d8210c3551bb3f4ade
naerok <chg9252@gmail.com>
2022-05-27 11:28:31 +0900

    spring넣은버전

---
+++ ex00/src/main/java/org/zerock/sample/Chef.java
@@ -0,0 +1,11 @@
+package org.zerock.sample;
+
+import org.springframework.stereotype.Component;
+
+import lombok.Data;
+
+@Component //스프링에게 해당 클래스가 스프링에서 관리해야 하는 대상임을 표시하는 어노테이션
+@Data
+public class Chef {
+
+}
