e64362ea8d9713c12623a0a86889849a90862d88
naerok <chg9252@gmail.com>
2022-05-29 16:05:17 +0900

    JUnit 오류 수정중

--- ex00/src/main/java/org/zerock/mapper/TimeMapper.java
+++ ex00/src/main/java/org/zerock/mapper/TimeMapper.java
@@ -6,7 +6,8 @@ public interface TimeMapper {
 	
 	// ;(세미콜론)이 없어야한다.
 	@Select("select sysdate from dual")
-	String getTime();
-
-	String getTime2();
+	public String getTime();
+	
+	
+	public String getTime2();
 }
